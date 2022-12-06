c6c742961faf226bf28627b04be07dbd0c4c1904
JeongYeChan <yechan0318>
2022-12-06 13:56:10 +0900

    Create Project

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+    <v-main>
+      <HelloWorld />
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  components: {
+    HelloWorld,
+  },
+});
+</script>
